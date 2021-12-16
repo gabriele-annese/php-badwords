422c56ecfe137e347b2718173213376754cf8717
gabriele-annese <gannese06@gmail.com>
2021-12-16 16:14:56 +0100

    add es censura la parola

--- index.php
+++ index.php
@@ -1,15 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <title>php-badwords</title>
+    <link rel="stylesheet" href="./css/main.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    <?php
-        $nome = 'michele'
-    ?>
-     <h1>ciao <?php echo  $nome ?></h1>
+   <div>
+       <h1>Censura la parola</h1>
+       <p>
+           <?php
+            $parolaCensurata = $_GET['censura'];
+            $text = "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ";
+            echo str_replace($parolaCensurata, "<span>***</span>", $text);
+            $lenghtText = strlen($text)
+           ?>
+       </p>
+       <span>Lunghezza del paragrafo: <?php echo $lenghtText ?></span>
+   </div>
 </body>
+
 </html>
